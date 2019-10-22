47f871129e20eee6a95f13d05d0983af4a150095
Daniel Vieira Makowiesky <dvm1993@hotmail.com>
2019-10-22 20:48:38 -0300

    cor de fundo

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import {View} from 'react-native';
-
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Title, Form, Input, Submit} from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Title>Repositorios</Title>
+      <Form>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Procurar Repositório"
+        />
+        <Submit onPress={() => {}}>
+          <Icon name="add" size={22} color="#FFF" />
+        </Submit>
+      </Form>
+    </Container>
+  );
 }
